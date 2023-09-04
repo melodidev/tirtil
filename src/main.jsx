e118c145d516fbf0795f0cc98d81deceff9e198f
Ezgi Okka <ezgiokka@gmail.com>
2023-09-04 13:16:51 +0300

    Enable strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { React, StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '/node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
